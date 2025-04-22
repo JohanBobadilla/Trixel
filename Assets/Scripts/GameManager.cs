2633badfaf7dac888cd60bde19ea1951a7bcce09
Johan Bobadilla <johanbobadillap@hotmail.com>
2025-04-22 14:22:54 -0500

    Delete TurnSystem

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,21 @@ public class GameManager : MonoBehaviour
 {
     public PlayerData[] players;
     public PlayerData currentPlayer;
+    private int currentPlayerTurn;
 
     void Start()
     {
-        currentPlayer = players[TurnSystem.instance.StartTurns()];
+        currentPlayer = players[StartTurns()];
         print(currentPlayer.playerName);
+    }
+    public int StartTurns()
+    {
+        currentPlayerTurn = Random.Range(0, 2);
+        return currentPlayerTurn;
+    }
+
+    public int ChangeTurn()
+    {
+        return currentPlayerTurn == 1 ? 0 : 1;
     }
 }
